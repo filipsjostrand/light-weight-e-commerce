9973405e1c15b8d0817b1e7ea6c510879e3955d7
filipsjostrand <filip_sjostrand@hotmail.com>
2025-11-12 15:28:14 +0100

    ADD Current project (productForm and product error) #addProduct

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -1,16 +1,21 @@
 "use client"
 
+// import { Button } from "@/components/ui/button";
 // import { formatCurrency } from "@/app/lib/formatters";
+
+import { Button } from "../../../../components/ui/button";
 import { formatCurrency } from "../../../lib/formatters";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
+import { addProduct } from "../../_actions/products";
 
 export function ProductForm() {
     const [priceInCents, setPriceInCents] = useState<number>()
 
     return (
-        <form className="space-y-8">
+        <form action={addProduct} className="space-y-8">
             <div className="space-y-2">
                 <Label htmlFor="name">Name</Label>
                 <Input type="text" id="name" name="name" required />
@@ -26,9 +31,22 @@ export function ProductForm() {
                 onChange={e => setPriceInCents(Number(e.target.value) || undefined)}
                 />
             </div>
-                    <div className="text-muted-foregound">
-            {formatCurrency((priceInCents || 0) / 100)}
-        </div>
+            <div className="text-muted-foregound">
+                {formatCurrency((priceInCents || 0) / 100)}
+            </div>
+            <div className="space-y-2">
+                <Label htmlFor="description">Description</Label>
+                <Textarea id="description" name="description" required />
+            </div>
+            <div className="space-y-2">
+                <Label htmlFor="file">File</Label>
+                <Input type="file" id="file" name="file" required />
+            </div>
+            <div className="space-y-2">
+                <Label htmlFor="image">Image</Label>
+                <Input type="file" id="image" name="image" required />
+            </div>
+            <Button type="submit">Save</Button>
         </form>
 
     )
--- src/app/admin/products/new/page.tsx
+++ src/app/admin/products/new/page.tsx
@@ -1,7 +1,7 @@
 import { PageHeader } from "../../_components/PageHeader";
 import { ProductForm } from "../_components/ProductForm";
 
-export default function NewProducPage() {
+export default function NewProductPage() {
     return <>
         <PageHeader>Add Product</PageHeader>
         <ProductForm />
